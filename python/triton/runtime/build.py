ab7efd754e2ffb718bb9cbb8b3ae1f65b54ee664
woctordho <woctordho@outlook.com>
2025-04-17 10:12:38 +0800

    Print cc_cmd when failed to compile in build.py

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -135,7 +135,13 @@ def _build(name: str, src: str, srcdir: str, library_dirs: list[str], include_di
         include_dirs += msvc_winsdk_inc_dirs
         library_dirs += msvc_winsdk_lib_dirs
     cc_cmd = _cc_cmd(cc, src, so, include_dirs, library_dirs, libraries, ccflags, language)
-    subprocess.check_call(cc_cmd, stdout=subprocess.DEVNULL)
+
+    try:
+        subprocess.check_call(cc_cmd)
+    except Exception as e:
+        print("Failed to compile. cc_cmd:", cc_cmd)
+        raise e
+
     return so
 
 
