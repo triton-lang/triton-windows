57261b98c9f99fc6adbe6bc94f19c1187d463085
woctordho <woctordho@outlook.com>
2026-02-08 15:01:30 +0800

    Add MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID

--- lib/Dialect/TritonInstrument/Transforms/FpSanitizer.cpp
+++ lib/Dialect/TritonInstrument/Transforms/FpSanitizer.cpp
@@ -2400,6 +2400,7 @@ struct ExternElementwisePattern
 class FpSanitizerPass
     : public impl::TritonInstrumentFpSanitizerBase<FpSanitizerPass> {
 public:
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(FpSanitizerPass)
   void runOnOperation() override {
     bool fpSanErrorEmitted = false;
     ScopedDiagnosticHandler diagnosticHandler(
