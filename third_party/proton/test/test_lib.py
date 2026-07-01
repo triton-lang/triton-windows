4afda4a40ff27554d879ba791d70497f2530610b
woctordho <woctordho@outlook.com>
2026-07-01 20:12:28 +0800

    Pass env var TRITON_CUPTI_LIB_PATH in test_lib.py

--- third_party/proton/test/test_lib.py
+++ third_party/proton/test/test_lib.py
@@ -8,7 +8,13 @@ import pathlib
 import pytest
 
 import triton._C.libproton.proton as libproton
-from triton.profiler.profile import _select_backend
+from triton.profiler.profile import _check_env, _select_backend
+
+
+def _select_backend_with_env() -> str:
+    backend = _select_backend()
+    _check_env(backend)
+    return backend
 
 
 def test_record():
@@ -40,7 +46,7 @@ def test_op():
 @pytest.mark.parametrize("source", ["shadow", "python"])
 def test_context(source: str, tmp_path: pathlib.Path):
     temp_file = tmp_path / "test_context.hatchet"
-    session_id = libproton.start(str(temp_file.with_suffix("")), source, "tree", _select_backend())
+    session_id = libproton.start(str(temp_file.with_suffix("")), source, "tree", _select_backend_with_env())
     depth = libproton.get_context_depth(session_id)
     libproton.finalize(session_id, "hatchet")
     assert depth >= 0
@@ -49,7 +55,7 @@ def test_context(source: str, tmp_path: pathlib.Path):
 
 def test_session(tmp_path: pathlib.Path):
     temp_file = tmp_path / "test_session.hatchet"
-    session_id = libproton.start(str(temp_file.with_suffix("")), "shadow", "tree", _select_backend())
+    session_id = libproton.start(str(temp_file.with_suffix("")), "shadow", "tree", _select_backend_with_env())
     libproton.deactivate(session_id, False)
     libproton.activate(session_id)
     libproton.finalize(session_id, "hatchet")
@@ -59,7 +65,7 @@ def test_session(tmp_path: pathlib.Path):
 
 def test_add_metrics(tmp_path: pathlib.Path):
     temp_file = tmp_path / "test_add_metrics.hatchet"
-    libproton.start(str(temp_file.with_suffix("")), "shadow", "tree", _select_backend())
+    libproton.start(str(temp_file.with_suffix("")), "shadow", "tree", _select_backend_with_env())
     id1 = libproton.record_scope()
     libproton.enter_scope(id1, "one")
     libproton.add_metrics(id1, {"a": 1.0, "b": 2.0, "veci": [1, 2, 3], "vecd": [1.0, 2.0, 3.0]})
