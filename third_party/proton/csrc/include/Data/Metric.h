bdd7845c788b0ed312624607f7d8e29549d374aa
woctordho <woctordho@outlook.com>
2026-07-01 19:56:26 +0800

    Release profiler metric buffer and reset metric buffer offset to fix issues with CUPTI CUDA graph callback

--- third_party/proton/csrc/include/Data/Metric.h
+++ third_party/proton/csrc/include/Data/Metric.h
@@ -476,6 +476,8 @@ public:
 
   ~MetricBuffer();
 
+  void release();
+
   void receive(const std::map<std::string, TensorMetric> &tensorMetrics,
                const std::map<std::string, MetricValueType> &scalarMetrics,
                const MetricKernelLaunchState &metricKernelLaunchState,
