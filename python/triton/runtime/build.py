8e5038900039e418ff26497d903262389e5cc312
woctordho <woctordho@outlook.com>
2026-07-29 13:58:10 +0800

    Add -DPy_GIL_DISABLED in build.py; consistently use C11 standard; do not add -D_Py_USE_GCC_BUILTIN_ATOMICS in TinyCC because it supports std atomics now

--- python/triton/runtime/build.py
+++ python/triton/runtime/build.py
@@ -112,6 +112,8 @@ def _cc_cmd(cc: str, src: str, out: str, include_dirs: list[str], library_dirs: 
             cc_cmd += ["/std:c11"]
         elif language == "c++":
             cc_cmd += ["/std:c++17"]
+        if sysconfig.get_config_var("Py_GIL_DISABLED"):
+            cc_cmd += ["/DPy_GIL_DISABLED"]
         cc_cmd += [f"/I{dir}" for dir in include_dirs if dir is not None]
         cc_cmd += [f"/Fo{out_base + '.obj'}"]
         cc_cmd += ["/link"]
@@ -123,16 +125,18 @@ def _cc_cmd(cc: str, src: str, out: str, include_dirs: list[str], library_dirs: 
     else:
         # for -Wno-psabi, see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=111047
         cc_cmd = [cc, src, "-O3", "-shared", "-Wno-psabi", "-o", out]
-        if language == "c++":
+        if language == "c":
+            cc_cmd.insert(3, "-std=c11")
+        elif language == "c++":
             cc_cmd.insert(3, "-std=c++17")
         if not (os.name == "nt" and is_clang(cc)):
             # Clang does not support -fPIC on Windows
             cc_cmd += ["-fPIC"]
-        if is_tcc(cc):
-            cc_cmd += ["-D_Py_USE_GCC_BUILTIN_ATOMICS"]
-        cc_cmd += [_library_flag(lib, cc) for lib in libraries]
-        cc_cmd += [f"-L{dir}" for dir in library_dirs]
+        if sysconfig.get_config_var("Py_GIL_DISABLED"):
+            cc_cmd += ["-DPy_GIL_DISABLED"]
         cc_cmd += [f"-I{dir}" for dir in include_dirs if dir is not None]
+        cc_cmd += [f"-L{dir}" for dir in library_dirs]
+        cc_cmd += [_library_flag(lib, cc) for lib in libraries]
     cc_cmd += ccflags
     return cc_cmd
 
@@ -181,12 +185,12 @@ def _build(name: str, src: str, srcdir: str, library_dirs: list[str], include_di
 
 
 def _library_flag(lib: str, cc: str) -> str:
-    if os.name == "nt" and not is_tcc(cc):
+    if is_msvc(cc) or is_clang_cl(cc):
         if not lib.lower().endswith(".lib"):
             lib = lib + ".lib"
         return lib
     # Match .so files with optional version numbers (e.g., .so, .so.1, .so.513.50.1)
-    if re.search(r'\.so(\.\d+)*$', lib) or lib.endswith(".a"):
+    if re.search(r'\.so(\.\d+)*$', lib) or lib.endswith((".a", ".lib")):
         return f"-l:{lib}"
     return f"-l{lib}"
 
