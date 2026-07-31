b3ed1c0e2e62bd8a04e0ec95ecf0f7d09130b7bb
Chao Zhang <zhangchao@microsoft.com>
2026-07-31 12:20:55 +0800

    fix test error

--- python/src/llvm.cc
+++ python/src/llvm.cc
@@ -673,7 +673,11 @@ void init_triton_llvm(py::module_ &m) {
       });
 
   // optimization levels
-  py::enum_<llvm::OptimizationLevel>(m, "optimization_level");
+  py::enum_<llvm::OptimizationLevel>(m, "optimization_level")
+      .value("O0", llvm::OptimizationLevel::O0)
+      .value("O1", llvm::OptimizationLevel::O1)
+      .value("O2", llvm::OptimizationLevel::O2)
+      .value("O3", llvm::OptimizationLevel::O3);
   m.attr("OPTIMIZE_O0") = llvm::OptimizationLevel::O0;
   m.attr("OPTIMIZE_O1") = llvm::OptimizationLevel::O1;
   m.attr("OPTIMIZE_O2") = llvm::OptimizationLevel::O2;
